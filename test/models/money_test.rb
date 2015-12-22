a6dd2136011a147c14ea46486a995ce057c87782
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-22 22:05:03 +0000

    Remove comment

--- test/models/money_test.rb
+++ test/models/money_test.rb
@@ -1,15 +1,11 @@
 require 'test_helper'
 
 class MoneyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "no negative balances" do
     Money.all.each do |m|
         assert m.balance > 0.0, "Balance(#{m.balance}) must be greater than or equal to £0.00"
     end
   end
-
 
 end
