94afb248c1c25eb2bb82678fae9507ca89c28ba2
Andy Stabler <andy_60@hotmail.co.uk>
2015-12-22 18:33:04 +0000

    Add money tests

--- test/models/money_test.rb
+++ test/models/money_test.rb
@@ -4,4 +4,12 @@ class MoneyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "no negative balances" do
+    Money.all.each do |m|
+        assert m.balance > 0.0, "Balance(#{m.balance}) must be greater than or equal to £0.00"
+    end
+  end
+
+
 end
